59bff7644217f41535a0ca38f67631ff4ccdd9ac
NikoBolt <docnikbolt@gmail.com>
2021-03-12 20:31:17 +0300

    Stage 2/6: Not just numbers

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -16,7 +16,7 @@ fun main(args: Array<String>) {
 }
 
 class Parser {
-    val strategy: Array<Any>
+    val arr: Array<Any>
     var _X: String = ""
     var _Y: String = ""
     var _Z: String = ""
@@ -26,7 +26,7 @@ class Parser {
     var sortedElements: Array<String> = emptyArray<String>()
 
     constructor(arg2: String) {
-        strategy = when (arg2) {
+        arr = when (arg2) {
             "long" -> arrayOf(1, "numbers", "greatest number: ")
             "line" -> arrayOf(2, "lines", "longest line: \n")
             else -> arrayOf(3, "words", "longest word: ")
@@ -35,104 +35,12 @@ class Parser {
         _X = countX()
         _Y = findY()
         _Z = countZ()
-        _P = calculateP()
+        _P = 100 / elements.size
         output(_X, _Y, _Z, _P)
     }
 
-    private fun calculateP(): Int {
-        return 100 / elements.size
-    }
-
-    private fun countZ(): String {
-        var counter = 0
-        for (i in 0 until elements.size) {
-            if (elements[i] == _Y) counter++
-        }
-        if (strategy[0] == 2) _Y += "\n"
-        return counter.toString()
-    }
-
-    private fun findY(): String {
-        bubleSort(elements)
-        return greatestElement()
-    }
-
-    fun greatestElement(): String = when(strategy[0]) {
-            1 -> sortedElements[sortedElements.size - 1]
-            2 -> sortedElements[sortedElements.size - 1]
-            else -> sortedElements[sortedElements.size - 1]
-    }
-
-    // O(n2) complexity
-    fun bubleSort(elements: Array<String>) {
-        sortedElements = elements
-
-        for (i in 0 until sortedElements.size) {
-            for (j in i + 1 until sortedElements.size) {
-                when (strategy[0]) {
-                    1 -> if (sortedElements[i].toInt() > sortedElements[j].toInt()) swap(i, j, sortedElements)
-                    else -> {
-                        if (sortedElements[i].length > sortedElements[j].length) {
-                            swap(i, j, sortedElements)
-                        }
-                        if (sortedElements[i].length == sortedElements[j].length) {
-                            compareStrings(i, j, sortedElements, 0)
-                        }
-                    }
-                }
-
-            }
-        }
-    }
-
-    private fun compareStrings(i: Int, j: Int, sortedElements: Array<String>, chr: Int) {
-        if (sortedElements[i][chr] > sortedElements[j][chr])
-            swap(i, j, sortedElements)
-        if (sortedElements[i][chr] == sortedElements[j][chr] && chr < sortedElements[i].length - 1)
-            compareStrings(i, j, sortedElements, chr + 1)
-    }
-
-    fun swap(a: Int, b: Int, array: Array<String>) {
-        val temp = array[b]
-        array[b] = array[a]
-        array[a] = temp
-    }
-
-    private fun countX(): String {
-        return when (strategy[0]) {
-            1 -> nomberOfIntegers(data)
-            2 -> nomberOfLines(data)
-            else -> nomberOfWords(data)
-        }
-    }
-
-    private fun nomberOfWords(data: Array<String>): String {
-        for (i in 0 until data.size) {
-            if (data[i] != " " && data[i] != "") {
-                elements += data[i]
-            }
-        }
-        return elements.size.toString()
-    }
-
-    private fun nomberOfLines(data: Array<String>): String {
-        for (i in 0 until data.size) {
-            elements += data[i]
-        }
-        return elements.size.toString()
-    }
-
-    fun nomberOfIntegers(data: Array<String>): String {
-        for (i in 0 until data.size) {
-            if (data[i] != " " && data[i] != "") {
-                elements += data[i]
-            }
-        }
-        return elements.size.toString()
-    }
-
     fun readInput(): Array<String> {
-        val space = " " // без пробелав в конце может последнее число не прочитать
+        val space = " " // без пробелав в конце может последнее число или word не прочитать
 
         val inputString: String =
             if (test) {
@@ -146,9 +54,7 @@ class Parser {
                 while (scanner.hasNext()) {
                     input += "\n" + scanner.nextLine()
                 }
-                if (strategy[0] == 1 || strategy[0] == 3) {
-                    input += space
-                }
+                if (arr[0] == 1 || arr[0] == 3) input += space
                 input
             }
         return stringToStringArray(inputString)
@@ -158,7 +64,7 @@ class Parser {
         var strings: Array<String> = emptyArray<String>()
         var num: String = ""
 
-        when (strategy[0]) {
+        when (arr[0]) {
             1 -> {
                 for (i in 0 until inputString.length) {
                     if (inputString[i].isDigit() || inputString[i] == '-' || inputString[i] == '_') {
@@ -191,8 +97,73 @@ class Parser {
         return strings
     }
 
+    private fun countX(): String {
+        for (i in 0 until data.size) {
+            when (arr[0]) {
+                1 -> if (data[i] != " " && data[i] != "") elements += data[i]
+                2 -> elements += data[i]
+                else -> if (data[i] != " " && data[i] != "") elements += data[i]
+            }
+        }
+        return elements.size.toString()
+    }
+
+    private fun findY(): String {
+        bubleSort(elements)
+        return greatestElement()
+    }
+
+    // O(n2) complexity
+    fun bubleSort(elements: Array<String>) {
+        sortedElements = elements
+
+        for (i in 0 until sortedElements.size) {
+            for (j in i + 1 until sortedElements.size) {
+                when (arr[0]) {
+                    1 -> if (sortedElements[i].toInt() > sortedElements[j].toInt()) swap(i, j, sortedElements)
+                    else -> {
+                        if (sortedElements[i].length > sortedElements[j].length) {
+                            swap(i, j, sortedElements)
+                        }
+                        if (sortedElements[i].length == sortedElements[j].length) {
+                            compareStrings(i, j, sortedElements, 0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun compareStrings(i: Int, j: Int, sortedElements: Array<String>, chr: Int) {
+        if (sortedElements[i][chr] > sortedElements[j][chr])
+            swap(i, j, sortedElements)
+        if (sortedElements[i][chr] == sortedElements[j][chr] && chr < sortedElements[i].length - 1)
+            compareStrings(i, j, sortedElements, chr + 1)
+    }
+
+    fun swap(a: Int, b: Int, array: Array<String>) {
+        val temp = array[b]
+        array[b] = array[a]
+        array[a] = temp
+    }
+
+    fun greatestElement(): String = when(arr[0]) {
+        1 -> sortedElements[sortedElements.size - 1]
+        2 -> sortedElements[sortedElements.size - 1]
+        else -> sortedElements[sortedElements.size - 1]
+    }
+
+    private fun countZ(): String {
+        var counter = 0
+        for (i in 0 until elements.size) {
+            if (elements[i] == _Y) counter++
+        }
+        if (arr[0] == 2) _Y += "\n"
+        return counter.toString()
+    }
+
     private fun output(_X: String, _Y: String, _Z: String, _P: Int) {
-        println("Total ${strategy[1]}: $_X.\n" +
-                "The ${strategy[2]}$_Y ($_Z time(s), $_P%).")
+        println("Total ${arr[1]}: $_X.\n" +
+                "The ${arr[2]}$_Y ($_Z time(s), $_P%).")
     }
 }
